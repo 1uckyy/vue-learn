4834a523082d49e92854f2b1a4a6f667318e09f0
Vladislav Novak <novak.vlad97@mail.ru>
2020-12-20 20:11:21 +0300

    complete

---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h2>Home page</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo non aliquid
+      iusto, ipsam quisquam pariatur assumenda optio iure fugiat harum.
+    </p>
+    <router-link to="/todos">Todos</router-link>
+  </div>
+</template>
